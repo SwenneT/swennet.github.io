1007ae572a1fe3461384392c0885e92a66e24fcb
Swen de Haan <swendh@gmail.com>
2018-06-27 16:27:00 +0200

    Introduction added & particles optimize in progress

---
+++ js/stars.js
@@ -0,0 +1,106 @@
+var fps = 20,
+	fpsAdjust = 60 / fps;
+
+particlesJS("particles-js", {
+  "particles": {
+	"fps_limit": fps,
+    "number": {
+      "value": 60,
+      "density": {
+        "enable": true,
+        "value_area": 800
+      }
+    },
+    "color": {
+      "value": "#ffffff"
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
+      },
+      "polygon": {
+        "nb_sides": 5
+      }
+    },
+    "opacity": {
+      "value": 0.5,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 1 * fpsAdjust,
+        "opacity_min": 0.1,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 5,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 40 * fpsAdjust,
+        "size_min": 3,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": false,
+      "distance": 150,
+      "color": "#ffffff",
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 0.3 * fpsAdjust,
+      "direction": "none",
+      "random": true,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": false,
+        "rotateX": 600,
+        "rotateY": 1200
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": false,
+        "mode": "repulse"
+      },
+      "onclick": {
+        "enable": false,
+        "mode": "push"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 400,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 400,
+        "size": 40,
+        "opacity": 8,
+		"duration": 2 * fpsAdjust,
+		"speed": 3 * fpsAdjust
+      },
+      "repulse": {
+        "distance": 200,
+        "duration": 0.4 * fpsAdjust
+      },
+      "remove": {
+        "particles_nb": 4
+      }
+    }
+  },
+  "retina_detect": true
+});
